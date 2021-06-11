20724854f90d430524b46358f406942b74b28b0c
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-11 08:38:55 +0700

    fix: move DAO to SleepDatabaseDao.kt

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,7 +16,57 @@
 
 package com.example.android.trackmysleepquality.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 
+// Notice that interface SleepDatabaseDao is annotated with @Dao.
+// All DAOs need to be annotated with the @Dao keyword.
 @Dao
-interface SleepDatabaseDao
+interface SleepDatabaseDao {
+    /**
+     * Inside the body of the interface, add an @Insert annotation.
+     * Below the @Insert, add an insert() function that takes an instance of the Entity class SleepNight as its argument.
+     */
+    @Insert
+    fun insert(night: SleepNight)
+
+    /**
+     * Add an @Update annotation with an update() function for one SleepNight.
+     * The entity that's updated is the entity that has the same key as the one that's passed in.
+     * You can update some or all of the entity's other properties.
+     */
+    @Update
+    fun update(night: SleepNight)
+
+    /**
+     * Add a @Query annotation with a get() function that takes a Long key argument and returns a nullable SleepNight.
+     * You'll see an error for a missing parameter.
+     */
+    // Add a String parameter to @Query that is an SQLite query to retrieve all the columns from a particular SleepNight entry.
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key") // Notice the :key. You use the colon notation in the query to reference arguments in the function.
+    fun get(key: Long): SleepNight?
+
+    /**
+     * Add another @Query with a clear() function and a SQLite query to DELETE everything from the daily_sleep_quality_table.
+     * This query does not delete the table itself.
+     */
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    /**
+     * Add a @Query with a getTonight() function.
+     * Make the SleepNight returned by getTonight() nullable, so that the function can handle the case where the table is empty.
+     * (The table is empty at the beginning, and after the data is cleared.)
+     */
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+
+    /**
+     * Add a @Query with a getAllNights() function
+     */
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+}
