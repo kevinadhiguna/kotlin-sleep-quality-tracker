2c5e90bb8ec79f770d5a9542e926c478ecec1c46
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-11 08:20:49 +0700

    refactor: create a SleepNight entity

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -16,3 +16,28 @@
 
 package com.example.android.trackmysleepquality.database
 
+import androidx.room.Entity
+
+/**
+ * SleepNight data class with :
+ * - parameters for an ID
+ * - a start time (in milliseconds)
+ * - an end time (in milliseconds)
+ * - a numerical sleep-quality rating.
+ */
+
+/**
+ * Before the class declaration, annotate the data class with @Entity.
+ * This annotation has several possible arguments.
+ * By default (no arguments to @Entity), the table name will be the same as the class.
+ * But let's use a helpful table name of daily_sleep_quality_table.
+ * This argument for the tableName is optional, but highly recommended.
+ */
+
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+    var nightId: Long = 0L,
+    val startTimeMilli: Long = System.currentTimeMillis(),
+    var endTimeMilli: Long = startTimeMilli,
+    var sleepQuality: Int = -1
+)
